7823230d785f134cd03e0c4f3e70fc81d78d592b
Simone Basso <bassosimone@gmail.com>
2021-12-29 13:43:59 +0100

    refactor(background.rs): make Manager generic wrt Child process
    Here I'm just trying to implement some common abstracting techniques
    where we reduce the amoung of code depending on side effects.
    
    In this case, we have "waiting on a running process", which is
    clearly a dependency on the external environment.
    
    By abstracting away this specific detail, we can make the background.rs
    code potentially easier to test in isolation.

--- src/background.rs
+++ src/background.rs
@@ -7,12 +7,23 @@ use std::sync::Mutex;
 
 /// adds a process to the pool of processes we manage.
 pub fn add(proc: Child) {
-    MANAGER.lock().unwrap().add(proc);
+    MANAGER.lock().unwrap().add(ChildWrapper { child: proc });
+}
+
+/// converts a deque of Child to a deque of ChildWrapper.
+fn to_child_wrapper(mut input: VecDeque<Child>) -> VecDeque<ChildWrapper> {
+    let mut output = VecDeque::<ChildWrapper>::new();
+    while input.len() > 0 {
+        output.push_back(ChildWrapper {
+            child: input.pop_front().unwrap(),
+        })
+    }
+    output
 }
 
 /// adds some processes to the pool of processes we manage.
 pub fn addq(procs: VecDeque<Child>) {
-    MANAGER.lock().unwrap().addq(procs);
+    MANAGER.lock().unwrap().addq(to_child_wrapper(procs));
 }
 
 /// collects terminated processes.
@@ -20,29 +31,45 @@ pub fn collect() {
     MANAGER.lock().unwrap().collect();
 }
 
-/// Manages background processes.
-struct Manager {
-    pub procs: VecDeque<Child>,
+/// Something that we could periodically check whether it has terminated.
+trait TryWaitable {
+    fn try_wait(&mut self) -> std::io::Result<Option<std::process::ExitStatus>>;
+}
+
+/// Wraps a Child process.
+struct ChildWrapper {
+    pub child: Child,
+}
+
+impl TryWaitable for ChildWrapper {
+    fn try_wait(&mut self) -> std::io::Result<Option<std::process::ExitStatus>> {
+        self.child.try_wait()
+    }
+}
+
+/// A Manager for background processes.
+struct Manager<T: TryWaitable> {
+    pub procs: VecDeque<T>,
 }
 
 /// The global process manager.
-static MANAGER: Lazy<Mutex<Manager>> = Lazy::new(|| Mutex::new(Manager::new()));
+static MANAGER: Lazy<Mutex<Manager<ChildWrapper>>> = Lazy::new(|| Mutex::new(Manager::new()));
 
-impl Manager {
+impl<T: TryWaitable> Manager<T> {
     /// creates a new process manager.
-    fn new() -> Manager {
+    fn new() -> Manager<T> {
         Manager {
             procs: VecDeque::<_>::new(),
         }
     }
 
     /// adds a process to the pool of processes we manage.
-    fn add(self: &mut Self, proc: Child) {
+    fn add(self: &mut Self, proc: T) {
         self.procs.push_back(proc);
     }
 
     /// adds some processes to the pool of processes we manage.
-    pub fn addq(self: &mut Self, mut procs: VecDeque<Child>) {
+    pub fn addq(self: &mut Self, mut procs: VecDeque<T>) {
         while procs.len() > 0 {
             self.add(procs.pop_front().unwrap()); // cannot fail
         }
@@ -50,7 +77,7 @@ impl Manager {
 
     /// collects terminated processes.
     fn collect(self: &mut Self) {
-        let mut running = VecDeque::<Child>::new();
+        let mut running = VecDeque::<_>::new();
         while self.procs.len() > 0 {
             let mut cur = self.procs.pop_front().unwrap(); // cannot fail
             match cur.try_wait() {
